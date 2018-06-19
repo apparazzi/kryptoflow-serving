805f40fb7f1c20f756f54e69f8698007c252ec55
Carlo Mazzaferro <carlo.mazzaferro@gmail.com>
2018-06-18 22:55:03 -0400

    Missing packages added, baseline code from kryptoflow re-added

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -1,10 +1,14 @@
-'use strict';
-
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
+  var app = new EmberApp(defaults, {
     // Add options here
+    emberHighCharts: {
+      includeHighCharts: false,
+      includeHighStock: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
